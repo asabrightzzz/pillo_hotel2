859df871e2e56d579dc97c25ca34dcf55b3fd52d
veriarajatunggal <veriarajatunggal@gmail.com>
2025-09-11 12:06:28 +0700

    benerin layout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ Route::view('/employees' , 'employees');
 Route::view('/facility' , 'facility');
 
 Route::resource('/reservation', ReservationController::class);
+
+// Menampilkan halaman utama
+Route::get('/reservation', [ReservationController::class, 'index']);
+// Menampilkan halaman edit dengan data reservation
+Route::get('/reservation/{reservation}/edit', [ReservationController::class, 'edit']);
+// Update data reservation
+Route::put('/reservation/{reservation}', [ReservationController::class, 'update']);
+// Delete data
+Route::delete('/reservation/{reservation}', [ReservationController::class, 'destroy']);
+// Store data atau insert data
+Route::post('/reservation/{reservation}', [ReservationController::class, 'store']);
