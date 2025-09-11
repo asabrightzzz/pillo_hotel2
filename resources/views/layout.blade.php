e42f61596efacded410507682619f4f12a6e787d
veriarajatunggal <veriarajatunggal@gmail.com>
2025-09-11 09:38:09 +0700

    first commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <html
   lang="en"
   class="layout-menu-fixed layout-compact"
-  data-assets-path="assets/vendor/"
+  data-assets-path="/assets/vendor/"
   data-template="vertical-menu-template-free"
 >
   <head>
@@ -13,7 +13,9 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>Analytics</title>
+    <title>
+      Analytics
+    </title>
 
     <meta name="description" content="" />
 
@@ -21,7 +23,7 @@
     <link
       rel="icon"
       type="image/x-icon"
-      href="assets/vendor/img/favicon/favicon.ico"
+      href="/assets/vendor/img/favicon/favicon.ico"
     />
 
     <!-- Fonts -->
@@ -34,38 +36,38 @@
 
     <link
       rel="stylesheet"
-      href="assets/vendor/vendor/fonts/iconify-icons.css"
+      href="/assets/vendor/vendor/fonts/iconify-icons.css"
     />
 
     <!-- Core CSS -->
-    <!-- build:css assets/vendor/vendor/css/theme.css  -->
+    <!-- build:css /assets/vendor/vendor/css/theme.css  -->
 
-    <link rel="stylesheet" href="assets/vendor/vendor/css/core.css" />
-    <link rel="stylesheet" href="assets/vendor/css/demo.css" />
+    <link rel="stylesheet" href="/assets/vendor/vendor/css/core.css" />
+    <link rel="stylesheet" href="/assets/vendor/css/demo.css" />
 
     <!-- Vendors CSS -->
 
     <link
       rel="stylesheet"
-      href="assets/vendor/vendor/libs/perfect-scrollbar/perfect-scrollbar.css"
+      href="/assets/vendor/vendor/libs/perfect-scrollbar/perfect-scrollbar.css"
     />
 
     <!-- endbuild -->
 
     <link
       rel="stylesheet"
-      href="assets/vendor/vendor/libs/apex-charts/apex-charts.css"
+      href="/assets/vendor/vendor/libs/apex-charts/apex-charts.css"
     />
 
     <!-- Page CSS -->
 
     <!-- Helpers -->
-    <script src="assets/vendor/vendor/js/helpers.js"></script>
+    <script src="/assets/vendor/vendor/js/helpers.js"></script>
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
 
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
 
-    <script src="assets/vendor/js/config.js"></script>
+    <script src="/assets/vendor/js/config.js"></script>
   </head>
 
   <body>
@@ -81,8 +83,98 @@
           <div class="app-brand demo">
             <a href="index.html" class="app-brand-link">
               <span class="app-brand-logo demo">
-                <img src="assets/images.jpeg" alt="Logo Pillo" width="70" />
+                <span class="text-primary">
+                  <svg
+                    width="25"
+                    viewBox="0 0 25 42"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                  >
+                    <defs>
+                      <path
+                        d="M13.7918663,0.358365126 L3.39788168,7.44174259 C0.566865006,9.69408886 -0.379795268,12.4788597 0.557900856,15.7960551 C0.68998853,16.2305145 1.09562888,17.7872135 3.12357076,19.2293357 C3.8146334,19.7207684 5.32369333,20.3834223 7.65075054,21.2172976 L7.59773219,21.2525164 L2.63468769,24.5493413 C0.445452254,26.3002124 0.0884951797,28.5083815 1.56381646,31.1738486 C2.83770406,32.8170431 5.20850219,33.2640127 7.09180128,32.5391577 C8.347334,32.0559211 11.4559176,30.0011079 16.4175519,26.3747182 C18.0338572,24.4997857 18.6973423,22.4544883 18.4080071,20.2388261 C17.963753,17.5346866 16.1776345,15.5799961 13.0496516,14.3747546 L10.9194936,13.4715819 L18.6192054,7.984237 L13.7918663,0.358365126 Z"
+                        id="path-1"
+                      ></path>
+                      <path
+                        d="M5.47320593,6.00457225 C4.05321814,8.216144 4.36334763,10.0722806 6.40359441,11.5729822 C8.61520715,12.571656 10.0999176,13.2171421 10.8577257,13.5094407 L15.5088241,14.433041 L18.6192054,7.984237 C15.5364148,3.11535317 13.9273018,0.573395879 13.7918663,0.358365126 C13.5790555,0.511491653 10.8061687,2.3935607 5.47320593,6.00457225 Z"
+                        id="path-3"
+                      ></path>
+                      <path
+                        d="M7.50063644,21.2294429 L12.3234468,23.3159332 C14.1688022,24.7579751 14.397098,26.4880487 13.008334,28.506154 C11.6195701,30.5242593 10.3099883,31.790241 9.07958868,32.3040991 C5.78142938,33.4346997 4.13234973,34 4.13234973,34 C4.13234973,34 2.75489982,33.0538207 2.37032616e-14,31.1614621 C-0.55822714,27.8186216 -0.55822714,26.0572515 -4.05231404e-15,25.8773518 C0.83734071,25.6075023 2.77988457,22.8248993 3.3049379,22.52991 C3.65497346,22.3332504 5.05353963,21.8997614 7.50063644,21.2294429 Z"
+                        id="path-4"
+                      ></path>
+                      <path
+                        d="M20.6,7.13333333 L25.6,13.8 C26.2627417,14.6836556 26.0836556,15.9372583 25.2,16.6 C24.8538077,16.8596443 24.4327404,17 24,17 L14,17 C12.8954305,17 12,16.1045695 12,15 C12,14.5672596 12.1403557,14.1461923 12.4,13.8 L17.4,7.13333333 C18.0627417,6.24967773 19.3163444,6.07059163 20.2,6.73333333 C20.3516113,6.84704183 20.4862915,6.981722 20.6,7.13333333 Z"
+                        id="path-5"
+                      ></path>
+                    </defs>
+                    <g
+                      id="g-app-brand"
+                      stroke="none"
+                      stroke-width="1"
+                      fill="none"
+                      fill-rule="evenodd"
+                    >
+                      <g
+                        id="Brand-Logo"
+                        transform="translate(-27.000000, -15.000000)"
+                      >
+                        <g
+                          id="Icon"
+                          transform="translate(27.000000, 15.000000)"
+                        >
+                          <g
+                            id="Mask"
+                            transform="translate(0.000000, 8.000000)"
+                          >
+                            <mask id="mask-2" fill="white">
+                              <use xlink:href="#path-1"></use>
+                            </mask>
+                            <use fill="currentColor" xlink:href="#path-1"></use>
+                            <g id="Path-3" mask="url(#mask-2)">
+                              <use
+                                fill="currentColor"
+                                xlink:href="#path-3"
+                              ></use>
+                              <use
+                                fill-opacity="0.2"
+                                fill="#FFFFFF"
+                                xlink:href="#path-3"
+                              ></use>
+                            </g>
+                            <g id="Path-4" mask="url(#mask-2)">
+                              <use
+                                fill="currentColor"
+                                xlink:href="#path-4"
+                              ></use>
+                              <use
+                                fill-opacity="0.2"
+                                fill="#FFFFFF"
+                                xlink:href="#path-4"
+                              ></use>
+                            </g>
+                          </g>
+                          <g
+                            id="Triangle"
+                            transform="translate(19.000000, 11.000000) rotate(-300.000000) translate(-19.000000, -11.000000) "
+                          >
+                            <use fill="currentColor" xlink:href="#path-5"></use>
+                            <use
+                              fill-opacity="0.2"
+                              fill="#FFFFFF"
+                              xlink:href="#path-5"
+                            ></use>
+                          </g>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </span>
               </span>
+              <span class="app-brand-text demo menu-text fw-bold ms-2"
+                >Sneat</span
+              >
             </a>
 
             <a
@@ -121,6 +213,7 @@
                     class="menu-link"
                   >
                     <div class="text-truncate" data-i18n="CRM">CRM</div>
+                    
                   </a>
                 </li>
                 <li class="menu-item">
@@ -132,6 +225,7 @@
                     <div class="text-truncate" data-i18n="eCommerce">
                       eCommerce
                     </div>
+                    
                   </a>
                 </li>
                 <li class="menu-item">
@@ -143,6 +237,7 @@
                     <div class="text-truncate" data-i18n="Logistics">
                       Logistics
                     </div>
+                    
                   </a>
                 </li>
                 <li class="menu-item">
@@ -152,6 +247,7 @@
                     class="menu-link"
                   >
                     <div class="text-truncate" data-i18n="Academy">Academy</div>
+                    
                   </a>
                 </li>
               </ul>
@@ -198,7 +294,24 @@
               class="navbar-nav-right d-flex align-items-center justify-content-end"
               id="navbar-collapse"
             >
-               <ul class="navbar-nav flex-row align-items-center ms-md-auto">
+              <!-- Search -->
+              <div class="navbar-nav align-items-center me-auto">
+                <div class="nav-item d-flex align-items-center">
+                  <span class="w-px-22 h-px-22"
+                    ><i class="icon-base bx bx-search icon-md"></i
+                  ></span>
+                  <input
+                    type="text"
+                    class="form-control border-0 shadow-none ps-1 ps-sm-2 d-md-block d-none"
+                    placeholder="Search..."
+                    aria-label="Search..."
+                  />
+                </div>
+              </div>
+              <!-- /Search -->
+
+              <ul class="navbar-nav flex-row align-items-center ms-md-auto">
+
                 <!-- User -->
                 <li class="nav-item navbar-dropdown dropdown-user dropdown">
                   <a
@@ -208,7 +321,7 @@
                   >
                     <div class="avatar avatar-online">
                       <img
-                        src="assets/vendor/img/avatars/1.png"
+                        src="/assets/vendor/img/avatars/1.png"
                         alt
                         class="w-px-40 h-auto rounded-circle"
                       />
@@ -221,7 +334,7 @@
                           <div class="flex-shrink-0 me-3">
                             <div class="avatar avatar-online">
                               <img
-                                src="assets/vendor/img/avatars/1.png"
+                                src="/assets/vendor/img/avatars/1.png"
                                 alt
                                 class="w-px-40 h-auto rounded-circle"
                               />
@@ -287,7 +400,6 @@
           <div class="content-wrapper">
             <!-- Content -->
             <div class="container-xxl flex-grow-1 container-p-y">
-              @yield('content')
             </div>
             <!-- / Content -->
 
@@ -303,7 +415,12 @@
                       document.write(new Date().getFullYear());
                     </script>
                     , made with ❤️ by
-                    <a href="" target="_blank" class="footer-link">Metschool</a>
+                    <a
+                      href=""
+                      target="_blank"
+                      class="footer-link"
+                      >Metschool</a
+                    >
                   </div>
                 </div>
               </div>
@@ -324,26 +441,26 @@
 
     <!-- Core JS -->
 
-    <script src="assets/vendor/vendor/libs/jquery/jquery.js"></script>
+    <script src="/assets/vendor/vendor/libs/jquery/jquery.js"></script>
 
-    <script src="assets/vendor/vendor/libs/popper/popper.js"></script>
-    <script src="assets/vendor/vendor/js/bootstrap.js"></script>
+    <script src="/assets/vendor/vendor/libs/popper/popper.js"></script>
+    <script src="/assets/vendor/vendor/js/bootstrap.js"></script>
 
-    <script src="assets/vendor/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="/assets/vendor/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
 
-    <script src="assets/vendor/vendor/js/menu.js"></script>
+    <script src="/assets/vendor/vendor/js/menu.js"></script>
 
     <!-- endbuild -->
 
     <!-- Vendors JS -->
-    <script src="assets/vendor/vendor/libs/apex-charts/apexcharts.js"></script>
+    <script src="/assets/vendor/vendor/libs/apex-charts/apexcharts.js"></script>
 
     <!-- Main JS -->
 
-    <script src="assets/vendor/js/main.js"></script>
+    <script src="/assets/vendor/js/main.js"></script>
 
     <!-- Page JS -->
-    <script src="assets/vendor/js/dashboards-analytics.js"></script>
+    <script src="/assets/vendor/js/dashboards-analytics.js"></script>
 
     <!-- Place this tag before closing body tag for github widget button. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
