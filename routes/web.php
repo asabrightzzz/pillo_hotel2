c0e6e62ec87acac31282cfc55cb345f09bf32e01
asabrightzzz <hannamariamonika123@gmail.com>
2025-09-11 11:47:42 +0700

    edited facility

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'layout')->name('home');
 
-Route::get('/', function () {
-    return view('facilities');
-});
-
 // Route::get('/', function () {
 //     return view('welcome');
 // });
@@ -17,5 +13,6 @@ Route::get('/', function () {
 Route::view('/guest', 'guest');
 
 Route::view('/employees' , 'employees');
+Route::view('/facility' , 'facility');
 
 Route::resource('/reservation', ReservationController::class);
