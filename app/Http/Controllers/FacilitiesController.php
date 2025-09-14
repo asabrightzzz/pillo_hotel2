e9286331e12bdfc5006a846fda2d5dfcea646d85
asabrightzzz <hannamariamonika123@gmail.com>
2025-09-14 18:23:04 +0700

    controller & route facilities

--- app/Http/Controllers/FacilitiesController.php
+++ app/Http/Controllers/FacilitiesController.php
@@ -8,26 +8,17 @@ use App\Models\Facility;
 
 class FacilitiesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $facilities = Facility::all();
         return view('admin.facilities.index', compact('facilities'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('admin.facilities.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -41,9 +32,7 @@ class FacilitiesController extends Controller
         return redirect()->route('facilities.index')->with('success', 'Fasilitas berhasil ditambahkan.');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit(Facility $facility)
     {
         return view('admin.facilities.edit', compact('facility'));
