915137168c498fe0e00917efc0dc8f5634736d45
Leeezoon <rehanzulhilmibudiman@gmail.com>
2025-09-16 10:15:19 +0700

    d

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ use App\Models\Reservation;
 use App\Http\Controllers\FacilityController;
 use App\Http\Controllers\GuestController;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Models\Guest;
 
 
@@ -23,35 +22,6 @@ use App\Models\Guest;
     Route::resource('employee', EmployeeController::class);
     
 });
-=======
-
-Route::view('/', 'layout')->name('home');
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::view('/guest', 'guest');
-
-Route::view('/employees' , 'employees');
-Route::view('/facility' , 'facility');
-
-// Satu Kesatuan dari semua method untuk kebutuhan CRUD
-Route::resource('/reservation', ReservationController::class);
-
-// Menampilkan halaman utama
-// Route::get('/reservation', [ReservationController::class, 'index']);
-// Menampilkan halaman edit dengan data reservation
-// Route::get('/reservation/{reservation}/edit', [ReservationController::class, 'edit']);
-// Update data reservation
-// Route::put('/reservation/{reservation}', [ReservationController::class, 'update']);
-// Delete data
-// Route::delete('/reservation/{reservation}', [ReservationController::class, 'destroy']);
-// Store data atau insert data
-// Route::post('/reservation/{reservation}', [ReservationController::class, 'store']);
-
-Route::resource('/roomcategories', RoomCategoriesController::class);
->>>>>>> f39f8339dc7c34d586175dbd49c46065d99089bf
 
 
 
