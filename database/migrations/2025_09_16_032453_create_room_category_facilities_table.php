9dd998ffd4f16863099db310ff5312c3350ba441
ghattanCrazyKiller <ghattanilhaq@gmail.com>
2025-09-16 10:43:07 +0700

    perubahan migration

--- database/migrations/2025_09_16_032453_create_room_category_facilities_table.php
+++ database/migrations/2025_09_16_032453_create_room_category_facilities_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('room_category_facilities', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
