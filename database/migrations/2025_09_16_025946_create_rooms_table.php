5716cded1f1067523f01490f89d375ba2a5afc29
Leeezoon <rehanzulhilmibudiman@gmail.com>
2025-09-16 10:06:16 +0700

    nambahin room dan room category table

---
+++ database/migrations/2025_09_16_025946_create_rooms_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('name');
+            $table->enum('status');
+            $table->enum('status');
+            $table->foreignId('room_category_id')->constrained('guests');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
