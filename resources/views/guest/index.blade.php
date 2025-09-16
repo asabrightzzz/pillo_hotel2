f3297bba7c1d3f15fb1c763b19b886579794fb94
ghattanCrazyKiller <ghattanilhaq@gmail.com>
2025-09-16 11:34:57 +0700

    membuat index room kaetgori faclity

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,28 +1,6 @@
 @extends('layout')
 
 @section('content')
-    <div class="col-xxl- mb-6 order-0">
-    <div class="col-xxl-12 mb-6 order-0">
-        <div class="card">
-            <div class="d-flex align-items-start row">
-                <div class="col-sm-7">
-                    <div class="card-body">
-                        <h5 class="card-title text-primary mb-3">Welcome, Admin! 🎉</h5>
-                        <p class="mb-6"> --Halaman data pelanggan
-                        </p>
-
-                        <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
-                    </div>
-                </div>
-                <div class="col-sm-5 text-center text-sm-left">
-                    <div class="card-body pb-0 px-0 px-md-6">
-                        <img src="../assets/img/illustrations/man-with-laptop.png" height="175" alt="View Badge User">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div class="col-xl">
       <form action="/guest" method="post">
         @csrf
