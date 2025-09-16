906f3589cc882a66537a1109df1b42bbee48f523
asabrightzzz <hannamariamonika123@gmail.com>
2025-09-16 10:31:43 +0700

    nambah + hapus banyak

--- database/migrations/2025_09_16_032337_create_room_reservations_table.php
+++ database/migrations/2025_09_16_032337_create_room_reservations_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('facility_controllers', function (Blueprint $table) {
+        Schema::create('room_reservations', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('facility_controllers');
+        Schema::dropIfExists('room_reservations');
     }
 };
