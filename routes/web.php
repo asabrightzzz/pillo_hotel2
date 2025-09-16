2a0508c6e07f88a85525ccd0105cd0021b6df179
ghattanCrazyKiller <ghattanilhaq@gmail.com>
2025-09-16 11:37:45 +0700

    memperbaiki routes

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Models\Reservation;
 use App\Http\Controllers\FacilityController;
 use App\Http\Controllers\GuestController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoomController;
 
 Route::view('/', 'layout')->name('home');
 
@@ -17,23 +18,18 @@ Route::view('/', 'layout')->name('home');
 
 Route::view('/guest', 'guest');
 
-<<<<<<< HEAD
     Route::resource('guest', GuestController::class);
     Route::resource('facility', FacilityController::class);
     Route::resource('reservation', ReservationController::class); 
     Route::resource('room', RoomController::class); 
     Route::view('/', 'layout')->name('home');
     Route::resource('employee', EmployeeController::class);
-    Route::get('/roomcategories', [RoomCategoriesController::class, 'index']);
-    Route::get('/roomcategories/{roomcategories}/edit', [RoomCategoriesController::class, 'edit']);
-    Route::put('/roomcategories/{roomcategories}', [RoomCategoriesController::class, 'update']);
-    Route::delete('/roomcategories/{roomcategories}', [RoomCategoriesController::class, 'destroy']);
-    Route::post('/roomcategories/{roomcategories}', [RoomCategoriesController::class, 'store']);
-});
-=======
-Route::view('/employees' , 'employees');
-Route::view('/facility' , 'facility');
->>>>>>> 749abc39f7a67b066268df8248e2137997474499
+    Route::get('/roomcategories', [RoomCategoryController::class, 'index']);
+    Route::get('/roomcategories/{roomcategories}/edit', [RoomCategoryController::class, 'edit']);
+    Route::put('/roomcategories/{roomcategories}', [RoomCategoryController::class, 'update']);
+    Route::delete('/roomcategories/{roomcategories}', [RoomCategoryController::class, 'destroy']);
+    Route::post('/roomcategories/{roomcategories}', [RoomCategoryController::class, 'store']);
+
 
 // Satu Kesatuan dari semua method untuk kebutuhan CRUD
 Route::resource('/reservation', ReservationController::class);
